7cd68023196893cfa3321dc246d28fe862e95e72
Jensen-holm <holmj@mail.gvsu.edu>
2023-03-01 10:53:55 -0500

    getting a different api to convert into yoda speak

--- yoda/get.py
+++ yoda/get.py
@@ -1,22 +1,27 @@
 import requests
 
-URL = "https://yodish.p.rapidapi.com/yoda.json"
+URL = "https://you-chat-gpt.p.rapidapi.com/"
 
-headers = {
+HEADERS = {
+    "content-type": "application/json",
     "X-RapidAPI-Key": "cd29d7a075mshb08572d8da1b1b4p153cd5jsn416ef45e8580",
-    "X-RapidAPI-Host": "yodish.p.rapidapi.com"
+    "X-RapidAPI-Host": "you-chat-gpt.p.rapidapi.com"
 }
 
 
-def translate(text: str) -> str:
+def translate(text: str):
+    payload = {
+        "question": f"Re-write this sentence as if yoda from star wars said it: '{text}'",
+        "max_response_time": 30
+    }
+
     r = requests.request(
         "POST",
         URL,
-        headers=headers,
-        params={
-            "text": text,
-        }
+        json=payload,
+        headers=HEADERS,
     )
+
     return r.text
 
 
