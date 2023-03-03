d7890da17b435b73d448bf50e31c825634109b88
Jensen-holm <holmj@mail.gvsu.edu>
2023-03-03 13:09:17 -0500

    making it a native cli app instead of a web app

--- src/components/Hello.tsx
+++
@@ -1,9 +0,0 @@
-import { Title } from "@mantine/core";
-
-export default function Hello() {
-    return (
-            <Title className="text-center first-letter:m-8">
-                Hello ! ! !
-            </Title>
-    )
-}
